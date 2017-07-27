fa9de8593f3962f224520e9ef747d51327539a37
胡贺东 <1182738042@qq.com>
2017-07-27 14:43:37 +0800

    修改原来的报错，

--- java/babycheck/src/main/java/com/guyue/guyueweb/GuyueWebApplication.java
+++ java/babycheck/src/main/java/com/guyue/guyueweb/GuyueWebApplication.java
@@ -1,15 +1,9 @@
 package com.guyue.guyueweb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={    
-	      JpaRepositoriesAutoConfiguration.class//禁止springboot自动加载持久化bean  
-	        })
 public class GuyueWebApplication{
 	
 	public static void main(String[] args) {
