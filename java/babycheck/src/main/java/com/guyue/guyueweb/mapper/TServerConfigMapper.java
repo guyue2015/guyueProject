ef509c4007aa228a227528ed6b35adf6b344bd64
HUHEDONG\Administrator <1182738042@qq.com>
2017-07-30 17:42:23 +0800

    删除slow项目，通过java实现更为简单

--- java/babycheck/src/main/java/com/guyue/guyueweb/mapper/TServerConfigMapper.java
+++
@@ -1,7 +0,0 @@
-package com.guyue.guyueweb.mapper;
-
-import com.guyue.guyueweb.common.CommonMapper;
-import com.guyue.guyueweb.mapperbean.TServerConfig;
-
-public interface TServerConfigMapper extends CommonMapper<TServerConfig> {
-}
