a832d71c59f551448dc29142d4c1f8bfd84a58cf
胡贺东 <1182738042@qq.com>
2017-08-02 13:43:04 +0800

    增加空包，默认方法拦截请求不开启

--- java/commonweb/src/main/java/com/guyue/commonweb/config/MybatisConfig.java
+++ java/commonweb/src/main/java/com/guyue/commonweb/config/MybatisConfig.java
@@ -8,14 +8,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.pagehelper.util.StringUtil;
 import com.guyue.commonweb.intercepptor.MybatisIntecepter;
 import com.guyue.commonweb.intercepptor.RequestRecordInterceptor;
 import com.guyue.commonweb.config.GuyueWebCommonConfig;
 
 @Configuration
 public class MybatisConfig{
-   @Value("${guyue.record.time.method.package}")
+   @Value("${guyue.record.time.method.package:com.guyue.commonweb.empty.*}")
    private String recordPackagePath;
+   /**
+    * 增加数据源拦截器
+     * @Title: guyueWebCommonConfigBean 
+     * @Description: 
+     * @param sqlSessionFactoryBean
+     * @return
+    */
    @Bean
    public GuyueWebCommonConfig guyueWebCommonConfigBean(@Qualifier("sqlSessionFactoryBean") SqlSessionFactoryBean sqlSessionFactoryBean){
 	   GuyueWebCommonConfig guyueWebBean = new GuyueWebCommonConfig();
@@ -27,6 +35,12 @@ public class MybatisConfig{
 	}
 		return guyueWebBean;
 	}
+   /***
+    * 增加方法运行拦截器
+     * @Title: setMymethodpoInt 
+     * @Description: 
+     * @return
+    */
    @Bean 
    public RegexpMethodPointcutAdvisor setMymethodpoInt(){
        RegexpMethodPointcutAdvisor methodPointAdvisor= new RegexpMethodPointcutAdvisor();
